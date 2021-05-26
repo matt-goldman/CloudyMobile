c05948b8fce5c807704e82800a640bc1f8bba4ea
Matt Goldman <mattgoldman1981@gmail.com>
2021-05-27 07:20:55 +1000

    reverted everything to standard config

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -6,17 +6,11 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.IO;
-using System.Security.Cryptography.X509Certificates;
 
 namespace CloudyMobile.Infrastructure
 {
     public static class ConfigureServices
     {
-        private static string WEBSITE_PRIVATE_CERTS_PATH { get; set; } = String.Empty;
-        private static string IDS_CERT_THUMBPRINT { get; set; } = String.Empty;
         public static IServiceCollection AddInfrastructureServices(
             this IServiceCollection services, 
             IConfiguration configuration,
@@ -32,34 +26,8 @@ namespace CloudyMobile.Infrastructure
             services.AddDefaultIdentity<ApplicationUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
-            if(environment.IsProduction())
-            {
-                // adding this compiler directive in addition to environment.IsProduction
-                // as the Nswag.MsBuild task fails without it. The Nswag task relies on
-                // Configuration rather than environment.
-#if !DEBUG
-                Console.WriteLine("Running in production environment.");
-
-                WEBSITE_PRIVATE_CERTS_PATH = configuration.GetValue<string>(nameof(WEBSITE_PRIVATE_CERTS_PATH));
-
-                IDS_CERT_THUMBPRINT = configuration.GetValue<string>(nameof(IDS_CERT_THUMBPRINT));
-
-                var certPath = Path.Combine(WEBSITE_PRIVATE_CERTS_PATH, $"{IDS_CERT_THUMBPRINT}.p12");
-
-                var certBytes = File.ReadAllBytes(certPath);
-
-                var cert = new X509Certificate2(certBytes);
-
-                services.AddIdentityServer()
-                    .AddApiAuthorization<ApplicationUser, ApplicationDbContext>()
-                    .AddSigningCredential(cert); 
-#endif
-            }
-            else
-            {
                 services.AddIdentityServer()
                     .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-            }
 
             services.AddAuthentication()
                 .AddIdentityServerJwt();
