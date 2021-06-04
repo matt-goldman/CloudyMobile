da5136e47954d56702763e7d6626948cb0ba789d
Matt Goldman <matt.goldman@outlook.com>
2021-06-04 10:02:22 +1000

    added data seed, added syncfusion autocomplete

--- src/Blazor/Program.cs
+++ src/Blazor/Program.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Syncfusion.Blazor;
 
 namespace CloudyMobile.Blazor
 {
@@ -15,6 +16,8 @@ namespace CloudyMobile.Blazor
     {
         public static async Task Main(string[] args)
         {
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDU1MDU1QDMxMzkyZTMxMmUzMFpyUzRmVnBIeFc3TzR4aXZGeURaU05ndnROTDBQYldyb2R2eStVRE92ajA9;NDU1MDU2QDMxMzkyZTMxMmUzMFM2WHBBNFBibS85ajQyQXhBWXVjQTZpa2VLdzVIZEc0VzZ2VVRKdEM4eXc9");
+
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
             Constants.BaseUrl = builder.HostEnvironment.BaseAddress;
@@ -37,6 +40,8 @@ namespace CloudyMobile.Blazor
             builder.Services.AddTransient<RecipesService>();
 
             builder.Services.AddApiAuthorization();
+
+            builder.Services.AddSyncfusionBlazor();
 
             await builder.Build().RunAsync();
         }
--- src/Infrastructure/Persistence/ApplicationDbContext.cs
+++ src/Infrastructure/Persistence/ApplicationDbContext.cs
@@ -6,7 +6,10 @@ using IdentityServer4.EntityFramework.Options;
 using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
 using System.Reflection;
+using System.Threading.Tasks;
 
 namespace CloudyMobile.Infrastructure.Persistence
 {
@@ -55,6 +58,47 @@ namespace CloudyMobile.Infrastructure.Persistence
                 Assembly.GetExecutingAssembly());
 
             base.OnModelCreating(builder);
+        }
+
+        public async Task SeedInitialData()
+        {
+            var haveStyles = await Styles.AnyAsync();
+            if (!haveStyles)
+            {
+                Console.WriteLine("Seeding initial data...");
+
+                var styles = new List<Style>
+                {
+                    new Style
+                    {
+                        Name = "Saison",
+                        Notes = "Saison is a pale ale that is highly carbonated, fruity, spicy, and often bottle conditioned. It was historically brewed with low alcohol levels, but modern productions of the style have moderate to high levels of alcohol. Along with several other varieties, it is generally classified as a farmhouse ale.",
+                        ImageUrl = "http://www.craftbeer.com/wp-content/uploads/_SF/thumbnails/american-brett.jpg"
+                    },
+                    new Style
+                    {
+                        Name = "Stout",
+                        Notes = "Dry stout is black beer with a dry-roasted character thanks to the use of roasted barley. The emphasis on coffee-like roasted barley and a moderate degree of roasted malt aromas define much of the character. Hop bitterness is medium to medium high. This beer is often dispensed via nitrogen gas taps that lend a smooth, creamy body to the palate.",
+                        ImageUrl = "http://www.craftbeer.com/wp-content/uploads/_SF/thumbnails/irish-style-dry-stout.jpg"
+                    },
+                    new Style
+                    {
+                        Name = "IPA",
+                        Notes = "Characterized by floral, fruity, citrus-like, piney or resinous American-variety hop character, the IPA beer style is all about hop flavor, aroma and bitterness. This has been the most-entered category at the Great American Beer Festival for more than a decade, and is the top-selling craft beer style in supermarkets and liquor stores across the U.S.",
+                        ImageUrl = "http://www.craftbeer.com/wp-content/uploads/_SF/thumbnails/american-india-pale-ale.jpg"
+                    },
+                    new Style
+                    {
+                        Name = "Lager",
+                        Notes = "Lager has little in the way of hop and malt character. A straw to gold, very clean and crisp, highly carbonated lager.",
+                        ImageUrl = "http://www.craftbeer.com/wp-content/uploads/_SF/thumbnails/american-lager.jpg"
+                    }
+                };
+
+                Styles.AddRange(styles);
+
+                await SaveChangesAsync();
+            }
         }
     }
 }
