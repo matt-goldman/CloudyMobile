8a1aa95c3efba39ca48800f504b534b1b1985183
Matt Goldman <matt.goldman@outlook.com>
2021-06-06 16:22:22 +1000

    added maui app from template

---
+++ src/MAUI/CloudyMobile.Maui/CloudyMobile.Maui/Android/MainActivity.cs
@@ -0,0 +1,10 @@
+﻿using Android.App;
+using Microsoft.Maui;
+
+namespace CloudyMobile.Maui
+{
+	[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true)]
+	public class MainActivity : MauiAppCompatActivity
+	{
+	}
+}
