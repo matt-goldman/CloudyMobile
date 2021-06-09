5e145fb86d91315ed66784408ad11f6296c06566
Matt Goldman <matt.goldman@outlook.com>
2021-06-10 06:02:07 +1000

    updated services

---
+++ src/MAUI/CloudyMobile.Maui/CloudyMobile.Maui/Services/BatchService.cs
@@ -0,0 +1,40 @@
+using CloudyMobile.Client;
+
+namespace CloudyMobile.Maui.Services
+{
+    public class BatchesService : BaseService
+    {
+        private RecipesClient recipesClient;
+
+        public RecipeService()
+        {
+            this.recipesClient = new RecipesClient(httpClient);
+        }
+
+        public async Task<int> AddRecipe(RecipeDto recipe)
+        {
+            return await recipesClient.CreateAsync(recipe);
+        }
+
+        public async Task<RecipeDto> GetRecipe(int id)
+        {
+            return await recipesClient.GetAsync(id);
+        }
+
+        public async Task<BeerStylesVm> GetBeerStyles()
+        {
+            return await recipesClient.StylesAsync();
+        }
+
+        public async Task<RecipeSearchResultsVm> SearchRecipes(string name, string style)
+        {
+            var query = new SearchRecipeQuery
+            {
+                Name = name,
+                Style = style
+            };
+
+            return await recipesClient.SearchAsync(query);
+        }
+    }
+}
--- src/MAUI/CloudyMobile.Maui/CloudyMobile.Maui/Services/RecipeService.cs
+++ src/MAUI/CloudyMobile.Maui/CloudyMobile.Maui/Services/RecipeService.cs
@@ -4,6 +4,37 @@ namespace CloudyMobile.Maui.Services
 {
     public class RecipeService : BaseService
     {
+        private RecipesClient recipesClient;
 
+        public RecipeService()
+        {
+            this.recipesClient = new RecipesClient(httpClient);
+        }
+
+        public async Task<int> AddRecipe(RecipeDto recipe)
+        {
+            return await recipesClient.CreateAsync(recipe);
+        }
+
+        public async Task<RecipeDto> GetRecipe(int id)
+        {
+            return await recipesClient.GetAsync(id);
+        }
+
+        public async Task<BeerStylesVm> GetBeerStyles()
+        {
+            return await recipesClient.StylesAsync();
+        }
+
+        public async Task<RecipeSearchResultsVm> SearchRecipes(string name, string style)
+        {
+            var query = new SearchRecipeQuery
+            {
+                Name = name,
+                Style = style
+            };
+
+            return await recipesClient.SearchAsync(query);
+        }
     }
 }
