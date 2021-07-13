78415bb8ac465a63ec96e751ac89107f6dc55c16
Matt Goldman <matt.goldman@outlook.com>
2021-07-14 08:41:14 +1000

    added platform page folders

---
+++ src/MAUI/CloudyMobile.Maui/CloudyMobile.Maui/Pages/Desktop/StatsPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+
+namespace CloudyMobile.Maui.Pages.Desktop
+{
+    public partial class StatsPage : ContentPage, IPage
+    {
+        public StatsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/MAUI/CloudyMobile.Maui/CloudyMobile.Maui/Pages/Tablet/LiveKegPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui;
+using Microsoft.Maui.Controls;
+using System;
+
+namespace CloudyMobile.Maui.Pages.Tablet
+{
+    public partial class LiveKegPage : ContentPage, IPage
+    {
+        public LiveKegPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
